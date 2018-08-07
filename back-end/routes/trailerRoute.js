e1855737312af425d7b90d90be130aa412d06ead
je-ni <jenniferolibie@gmail.com>
2018-08-07 13:31:09 +0100

    added the trailer routings

--- back-end/routes/trailerRoute.js
+++ back-end/routes/trailerRoute.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 var trailerController = require('../Controllers/trailerController');
 
-/* GET comments listing. */
-router.get('/', trailerController.get);
+/* GET trailers listing. */
+router.get('/', trailerController.getAll);
 
+router.get('/:id', trailerController.getById);
+
+router.post('/create', trailerController.add);
+
+router.delete('/delete/:id', trailerController.delete);
+
+router.get('/search', trailerController.search);
 
 module.exports = router;
