d0f81057dd449ac9d4cdce89f099e63407a9a301
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-07 08:40:26 +0100

    updated and fixed the addmoviecategory function

--- back-end/Services/movieCategoryService.js
+++ back-end/Services/movieCategoryService.js
@@ -4,13 +4,13 @@ var repo = require('../Repositories/movieCategoryRepo');
 
 exports.addCategory = function(req, res, data){
     repo.add(data, function(err, Category){
-        if(err) res.json({err: err, message: "Something went wrong, please try again"});
-        res.json({message: 'Category added successfully'});
+        if(err) res.json({err: err, message: "Something w{ent wrong, please try again"});
+        res.json({message: 'Category added successfully', data: data});
     });   
 }
 
 exports.getAllCategories = function(req, res){
-    repo.get({}, '-__v','', /*{path: 'subscribers', select: '-preferences -__v'}*/'', function(err, Categories){
+    repo.get({}, '-__v','', {path: 'subscribers', select: '-preferences -__v'}, function(err, Categories){
         if(err) res.json({err: err, message: 'Something went wrong'});
         res.json(Categories);
     });
