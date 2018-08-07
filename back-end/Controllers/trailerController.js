0a54e0faf5fd54b96348b1c4a82e4dd37cb6ceb1
je-ni <jenniferolibie@gmail.com>
2018-08-07 13:30:42 +0100

    added all function trailer controller

--- back-end/Controllers/trailerController.js
+++ back-end/Controllers/trailerController.js
@@ -1,5 +1,32 @@
 var service = require('../Services/trailerService');
 
-exports.get = function(req, res){
-    return service.get(req, res);
+exports.add = function(req, res){
+    data = {
+        title: req.body.title,
+        description: req.body.description,
+        rating: req.body.rating,
+        year: req.body.year,
+        productionCompany: req.body.productionCompany,
+        duration: req.body.duration,
+    }
+    return service.add(req, res, data);
+}
+
+exports.getAll = function(req, res){
+    return service.getAll(req, res);
+}
+
+exports.getById = function(req, res){
+    var id = req.params.id;
+    return service.getById(req, res, id);
+}
+
+exports.search = function(req, res){
+    var option = req.query;
+    return service.search(req, res, option);
+}
+
+exports.delete = function(req, res){
+    var option = {_id: req.params.id};
+    return service.delete(req, res, option);
 }
