51db27df4f3f7b9ad5c18a692806931900c9ce84
je-ni <jenniferolibie@gmail.com>
2018-08-07 08:42:21 +0100

    defining trailer model

--- back-end/Models/trailerModel.js
+++ back-end/Models/trailerModel.js
@@ -2,16 +2,16 @@ var mongoose = require('mongoose');
 
 var TrailerSchema = mongoose.Schema({
     title: String,
-    // trailer_video: {type: VideoTrack, unique: true},
     description: String,
     rating: Number,
     year: Number,
     productionCompany: String,
     duration: Number,
-    // movieCover: File, 
-    //casts: [{name:--, role:--, etc}],
     categories: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
     comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+    // trailer_video: {type: VideoTrack, unique: true},
+    // movieCover: File, 
+    //casts: [{name:--, role:--, etc}],
 })
 
 module.exports = mongoose.model('Trailer', TrailerSchema);
