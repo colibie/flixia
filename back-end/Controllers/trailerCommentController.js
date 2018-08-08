4368cf4b88bdd78be0eeea9e8ef32512f5f23f95
je-ni <jenniferolibie@gmail.com>
2018-08-08 16:56:22 +0100

    added trailer comment controller

---
+++ back-end/Controllers/trailerCommentController.js
@@ -0,0 +1,20 @@
+var service = require('../Services/trailerService');
+
+exports.add = function(req, res){
+    data = {
+        time: Date.now(),
+        content: req.body.content,
+        rating: req.body.rating,
+        trailer: req.body.trailer,
+    }
+    return service.add(req, res, data);
+}
+
+exports.getAll = function(req, res){
+    return service.getAll(req, res);
+}
+
+exports.delete = function(req, res){
+    var option = {_id: req.params.id};
+    return service.delete(req, res, option);
+}
--- back-end/JoiSchema/trailerCommentSchema.js
+++ back-end/JoiSchema/trailerCommentSchema.js
@@ -2,8 +2,7 @@ var Joi = require('joi');
 
 module.exports = Joi.object().keys({
     time: Joi.any(),
-    description: Joi.string(),
+    content: Joi.string(),
     rating: Joi.number().integer().min(0).max(5),
-    year: Joi.number().integer(),
     trailer: Joi.string().alphanum().length(24)
 });
