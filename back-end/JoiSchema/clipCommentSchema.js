412bd83e138f00d9d88428bf60519eb00ad996c3
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-08 14:27:37 +0100

    validated the comment properties using joi

--- back-end/JoiSchema/clipCommentSchema.js
+++ back-end/JoiSchema/clipCommentSchema.js
@@ -1,1 +1,7 @@
 var Joi = require('joi');
+
+module.exports = Joi.object().keys({
+    content: Joi.string().required(),
+    rating: Joi.number().integer().min(0).max(5),
+    time: Joi.number().integer(),
+});
