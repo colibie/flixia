3808ff05c754e13884f3afb85084ef62e2060c6f
je-ni <jenniferolibie@gmail.com>
2018-08-08 22:33:13 +0100

    corrected email validation

--- back-end/JoiSchema/userSchema.js
+++ back-end/JoiSchema/userSchema.js
@@ -2,7 +2,7 @@ var Joi = require('joi');
 
 module.exports = Joi.object().keys({
     userName: Joi.string().required(),
-    email: Joi.email().required(),
+    email: Joi.string().email(),
     password: Joi.string().required(),
     profilePicture: Joi.any(),
 });
