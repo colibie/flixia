c8bb22688538b0fcf76543304aad263c883644b2
Joshe <femijay9@gmail.com>
2018-08-08 21:20:35 +0200

    currently working on home, cleared redundant data

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,56 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import './home.css';
 import '../general_style.css'
-import PasswordComponent from '../components/password';
 // import Note from '../components/note'
 import GuttersGrid from '../components/cardSet'
-
+import MovieTab from '../components/movTab.js'
 
 const Home = () => {
     return (
         <div>
-            <div>
-                <Header 
-                    logo="Best"
-                    // the data represents the header data and their routes link
-                    // please check the app.js to make sure the link exists in the 
-                    // Routing file
-                    data={[
-                        {
-                            name: 'Contact',
-                            link: '/contact'
-                        },
-                        {
-                            name: 'About',
-                            link: '/about'
-                        },
-                        {
-                            name: 'Home',
-                            link: '/'
-                        }
-                    ]} 
-                />
-                
+            <div className="header">
+            {/* //header goes here */}
+                <Header />
             </div>
             <div className="jumbotron general">
-                <div className="container fluid">
-                    <h1> In skyfall, this is the </h1>
-                    <h1> <NavLink to="/contact"> Contact</NavLink> </h1>
-                    <span> <NavLink to="/about"> About </NavLink></span> 
-                </div>
+                 {/* the slider goes here */}
             </div>
-            <span className="col-md-sm">
-                <input type="text" className="text" placeholder="Enter your name over here" />
-                <button className="btn btn-lg btn-success">Oluronbi</button>
-            </span>
-            {/* <Note /> */}
-            <PasswordComponent />
+            <MovieTab />
+            {/* movie cards grid comes here */}
             <GuttersGrid />
             <hr />
-            <Footer />
+            {/* page footer goes here */}
+                <Footer /> 
         </div>
     )
 }
