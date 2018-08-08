242a94a004fb7efe3e76dccccbacf0cefade21a2
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-08 11:40:38 +0100

    created and tested celebrity feature

--- back-end/JoiSchema/celebritySchema.js
+++ back-end/JoiSchema/celebritySchema.js
@@ -3,6 +3,6 @@ var Joi = require('joi');
 module.exports = Joi.object().keys({
     name: Joi.string().required(),
     biography: Joi.string(),
-    dateOfBirth: Joi.number().integer(),
-    movieIndurtyRole: Joi.string(),
+    dob: Joi.number().integer(), //dob stands for date of birth
+    //movieIndurtyRole: Joi.string(),
 });
