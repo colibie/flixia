4dcbed4da51383be1f1cf455147db6eb9d57824f
je-ni <jenniferolibie@gmail.com>
2018-08-09 15:06:05 +0100

    merged movie category service

---
+++ back-end/Services/movieCategoryService.js
@@ -0,0 +1,28 @@
+//This is the section that takes care of all the logical functionality of adding and getting movie categories
+var repo = require('../Repositories/movieCategoryRepo');
+var baseService = require('../Services/baseService'); //contains the content of module.exports
+var joiSchema = require('../JoiSchema/movieCategoriesSchema');
+
+
+function movieCategoryService(joiSchema){
+    //must be added for population purposes
+    this.structure = '-__v';
+    this.populateA = '';
+    this.populateB = '';
+    //needed to define the joiSchema
+    this.joiSchema = joiSchema;
+}
+
+movieCategoryService.prototype = baseService(repo);
+
+module.exports = new movieCategoryService(joiSchema);
+
+
+
+
+
+
+
+
+
+
