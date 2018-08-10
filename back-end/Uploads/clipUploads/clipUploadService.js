67218723767d0fd180b18ef2cd7a3dc10a8fdf8f
je-ni <jenniferolibie@gmail.com>
2018-08-10 10:08:24 +0100

    modified clip upload service

---
+++ back-end/Uploads/clipUploads/clipUploadService.js
@@ -0,0 +1,30 @@
+//process for uploading images
+var multer = require('multer');
+
+//adjust how files are stored
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './Uploads/clipUploads/clipVideos/');
+    },
+    filename: function(req, file, callback){
+        callback(null, 'cVideo_'+ file.originalname);
+    }
+});
+
+var fileFilter = function(req, file, callback){
+    if (file.mimetype === 'video/mp4'){
+        //accept a file
+        callback(null, true);
+    } else {
+        //reject a file
+        callback(new Error('Clip upload failed. Supports only mp4'), false);
+    }    
+}
+
+exports.upload = multer({
+    storage: storage, 
+    limits: { 
+        fileSize: 1024 * 1024 * 250
+    },
+    fileFilter: fileFilter
+});
