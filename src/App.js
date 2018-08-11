44c37db8ff35cc2146214045b1989be50eaf5ca1
Joshe <femijay9@gmail.com>
2018-08-11 20:37:27 +0200

    added routes to signin, login and t&c's

--- src/App.js
+++ src/App.js
@@ -7,21 +7,27 @@ import FAQ from './Pages/faq/FAQ';
 import Celebs from './Pages/celebs/Celebs';
 import Error from './Pages/error/Error';
 import Terms_and_Conditions from './Pages/terms/tandc'
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/Signup/Signup';
+// import SignUpModal from './Pages/components/signupModal';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-		<Switch>
-			<Route path="/" component={Home} exact/>
-			<Route path="/about" component={About} />
-			<Route path="/movies" component={Movies} />
-			<Route path="/faq" component={FAQ} />
-			<Route path="/celebs" component={Celebs} />
-			<Route path="*" component={Error} />
-			<Route path="/terms" component={Terms_and_Conditions} />
-		</Switch>
-      </BrowserRouter>
+				<Switch>
+					<Route path="/" component={Home} exact/>
+					<Route path="/about" component={About} />
+					<Route path="/movies" component={Movies} />
+					<Route path="/faq" component={FAQ} />
+					<Route path="/celebs" component={Celebs} />
+					<Route path="/terms" component={Terms_and_Conditions} />
+					<Route path="/login" component={Login} />
+					<Route path="/signup" component={SignUp} />
+					<Route path="*" component={Error} />
+					{/* <Route path="/signupModal" component={SignUpModal} /> */}
+				</Switch>
+			</BrowserRouter>
     );
   }
 }
