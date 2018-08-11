1cbf771749de5f2a6444603981083c28cb1ecb81
Joshe <femijay9@gmail.com>
2018-08-11 20:36:34 +0200

    imported semantic-ui css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css' 
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
