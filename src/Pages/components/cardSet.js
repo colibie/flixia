96bcfa2ce48a5c2eb2998fb3e2fc24af50c00ac8
Joshe <femijay9@gmail.com>
2018-08-13 14:01:36 +0200

    changed orientation and sizing of cards

--- src/Pages/components/cardSet.js
+++ src/Pages/components/cardSet.js
@@ -3,46 +3,83 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Icon from '@material-ui/icons'
+import './cardSet.css'
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    height: 340,
-    width: 270,
+    height: 330,
+    width: 260,
+    padding: 10,
   },
   control: {
     padding: theme.spacing.unit * 2,
   },
+  paperImage: {
+    height: '85%',
+    width: '100%',
+    borderRadius: '8',
+    marginBottom: '10px',
+  },
+  paperContent: {
+    height: '20%',
+    width: '100%',
+    display: 'inline-flex',
+  },
+  paperContent1: {
+    width: '100%',
+    height: '50%'
+  },
+  paperContent2: {
+    width: '100%',
+    height: '50%',
+    float: 'left'
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    float: 'left'
+  }
 });
 
 class GuttersGrid extends React.Component {
+  state = {
+    spacing: '16',
+  };
+
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value,
+    });
+  };
 
   render() {
     const { classes } = this.props;
+    const { spacing } = this.state;
 
     return (
       <Grid container className={classes.root} spacing={16}>
         <Grid item xs={12}>
-          <Grid container className={classes.demo} justify="center" spacing={16}>
-            {[0, 1, 2, 3,4,5,6,7,8,9].map(value => (
+          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
+            {[0,0,0,0,0,0,0,0,0,0,0,0].map(value => (
               <Grid key={value} item>
                 <Paper className={classes.paper}>
-                    <div style={{height: '73%'}}>
-                        <img alt="" src="" />
-                    </div>
-                    <div style={{height: "27", padding:"20px"}}>
-                        <h4><b> her name</b></h4>
-                        <p>{}<Icon>star</Icon>What she is all about</p>
-                    </div>
+                  <div className={classes.paperImage}>
+                  <img src="" className={classes.image} alt=""/>
+                  </div>
+                  <div>
+                      Movie Title <br />
+                      <span><h3 className="fa fa-star icon"></h3> Likes and <b className="fa fa-comments icon"></b> Comments </span>
+                  </div>
                 </Paper>
+                <h1 className="fa fa-loading "></h1>
               </Grid>
             ))}
           </Grid>
         </Grid>
-       </Grid>
+      </Grid>
     );
   }
 }
