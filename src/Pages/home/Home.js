fbfda68941f480017c11ef9c442e81590afb351c
Joshe <femijay9@gmail.com>
2018-08-13 14:15:31 +0200

    refactored and fixed some code-jam bugs

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -3,23 +3,23 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import './home.css';
 import '../general_style.css'
-// import Note from '../components/note'
-import GuttersGrid from '../components/cardSet'
 import MovieTab from '../components/movTab.js'
+
+
 
 const Home = () => {
     return (
         <div>
-            <div className="header">
+            {/* <div className="header"> */}
             {/* //header goes here */}
                 <Header />
-            </div>
+            {/* </div> */}
             <div className="jumbotron general">
                  {/* the slider goes here */}
+                
             </div>
             <MovieTab />
             {/* movie cards grid comes here */}
-            <GuttersGrid />
             <hr />
             {/* page footer goes here */}
                 <Footer /> 
