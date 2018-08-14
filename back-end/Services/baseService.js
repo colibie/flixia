2573adb2c594aa53a6afc67b55bc03ed06b9ec8c
je-ni <jenniferolibie@gmail.com>
2018-08-14 16:27:10 +0100

    resolved merge conflict

--- back-end/Services/baseService.js
+++ back-end/Services/baseService.js
@@ -1,5 +1,7 @@
 var validator = require('../JoiSchema/validator');
 
+var bcrypt = require('bcrypt');
+var token = require('../Config/jwt');
 //creating a constructor for base Services
 function BaseService(repo){
     if(!repo) throw new Error("A repo must be provided");
@@ -69,6 +71,32 @@ BaseService.prototype.update = function(req, res, id, options){
     this.repo.update(id, options, function(err, update){
         if(err) res.json({err: err, message: `The data could not be updated`});
         res.json({message: update});
+    });
+}
+
+BaseService.prototype.login = function(req, res, options, data){
+    this.repo.get(options, '','','', function(err, result){
+        if (result.length < 1){
+            res.status(401).json({message: 'Email/Password is incorrect'});
+        } else if(result.length >= 1){
+            bcrypt.compare(data.password, result[0].password, function(err, success){
+                if(err) res.status(401).json({error: err, message: 'Email/Password is incorrect'});
+                else if (success) {
+                    res.status(200).json({
+                        message: 'Welcome and enjoy your stay',
+                        token: token({
+                            email: result[0].email,
+                            id: result[0]._id
+                        }),
+                    });
+                }
+                else {
+                    res.status(401).json({message: 'Email/Password is incorrect' });
+                }
+            });
+        }else{
+            res.status(500).json({message: 'Email/Password is incorrect'});
+        }
     });
 }
 
