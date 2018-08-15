29a0a366213a3fd2f69bccf34e14464d11c922af
Joshe <femijay9@gmail.com>
2018-08-15 16:29:19 +0200

    removed dummy routes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Movies from './Pages/movies/Movies';
 import FAQ from './Pages/faq/FAQ';
 import Celebs from './Pages/celebs/Celebs';
 import Error from './Pages/error/Error';
-import Terms_and_Conditions from './Pages/terms/tandc'
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/Signup/Signup';
-// import SignUpModal from './Pages/components/signupModal';
+import Terms_and_Conditions from './Pages/terms/tandc';
 
 class App extends Component {
   render() {
@@ -22,10 +19,7 @@ class App extends Component {
 					<Route path="/faq" component={FAQ} />
 					<Route path="/celebs" component={Celebs} />
 					<Route path="/terms" component={Terms_and_Conditions} />
-					<Route path="/login" component={Login} />
-					<Route path="/signup" component={SignUp} />
 					<Route path="*" component={Error} />
-					{/* <Route path="/signupModal" component={SignUpModal} /> */}
 				</Switch>
 			</BrowserRouter>
     );
