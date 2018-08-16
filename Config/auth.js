f87b727098450777deb27d83edc56feed90b775f
je-ni <jenniferolibie@gmail.com>
2018-08-16 18:03:48 +0100

    changed the app

--- Config/auth.js
+++ Config/auth.js
@@ -1,8 +1,8 @@
 
 exports.facebookAuth = {
-    'clientID'      : '1310150985788548', // App ID
-    'clientSecret'  : '86572fa24275ee9375b53e0afb94713b', // App Secret
-    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+    'clientID'      : '270789443749446', // App ID
+    'clientSecret'  : 'b1ddce6ae73d0bebc7fc922e72fe4069', // App Secret
+    'callbackURL'   : 'https://flixia.herokuapp.com/auth/facebook/callback',
     'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
