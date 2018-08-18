fca108d8ffc2b46ba31ae2cd7b3f38611d9d2d2b
je-ni <jenniferolibie@gmail.com>
2018-08-18 01:12:58 +0100

    added cloudary upload configuration

---
+++ Config/cloudinary.js
@@ -0,0 +1,43 @@
+var cloudinary = require('cloudinary');
+
+//promise is used here to ensure that file uploads before the next function happens
+exports.addTrailerCover = function(filename){
+    return new Promise(resolve => {
+        cloudinary.uploader.unsigned_upload(filename, {resource_type: 'image'}, 'kpxzsgjw', function(result){
+            resolve({url: result.url, ID: result.public_id});
+        });
+    }, reject => {
+        reject({message: 'File could not be uploaded'});
+    });
+}
+
+exports.addTrailerVideo = function(filename){
+    return new Promise(resolve => {
+        cloudinary.uploader.unsigned_upload(filename, {resource_type: 'video'},'djo8romk', function(result){
+            resolve({url: result.url, ID: result.public_id});
+        });
+    }, reject => {
+        reject({message: 'File could not be uploaded'});
+    });
+}
+
+//promise is used here to ensure that file uploads before the next function happens
+exports.addClipUpload = function(filename){
+    return new Promise(resolve => {
+        cloudinary.uploader.unsigned_upload(filename,{resource_type: 'video'} ,'v6ebp55w', function(result){
+            resolve({url: result.url, ID: result.public_id});
+        });
+    }, reject => {
+        reject({message: 'File could not be uploaded'});
+    });
+}
+
+exports.deleteUpload = function(publicId){
+    return new Promise(function(resolve){
+        cloudinary.uploader.destroy(publicId, function(result){
+            resolve(result);
+        });
+    }, reject => {
+        reject({message: 'File could not be deleted'});
+    });
+}
