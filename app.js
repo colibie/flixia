8decf246eb94e2ee83693ebe993a7e4e3d0b21cd
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-25 16:11:39 +0100

    passed the admin router inside app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var clipsRouter = require('./routes/clipRoute');
 var clipCommentRouter = require('./routes/clipCommentRoute');
 var rolesRouter = require('./routes/roleRoute')
 var celebrityRouter = require('./routes/celebrityRoute');
+var adminRouter = require('./routes/adminRoute');
 
 var app = express();
 
@@ -57,6 +58,7 @@ app.use('/roles', rolesRouter);
 app.use('/clips', clipsRouter);
 app.use('/clipComments', clipCommentRouter);
 app.use('/celebrities', celebrityRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
