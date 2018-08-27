3e63b96afe2c1d2423978ad0ec5021a586becfc5
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-27 11:57:57 +0100

    implemented try catch in the admin service

--- Services/adminService.js
+++ Services/adminService.js
@@ -16,26 +16,40 @@ function adminService(joiSchema){
 adminService.prototype = baseService(repo);
 
 adminService.prototype.uploadPicture = function(req, res, data){
-    repo.update(data._id, {profilePicture: data.profilePicture}, function(err, admin){
-        if(err) res.json({err: err, message: `The admin could not be updated`});
-        res.json({message: 'Profile picture uploaded successfully'});
-    });
+    try{ 
+            repo.update(data._id, {profilePicture: data.profilePicture}, function(err, admin){
+            if(err) res.json({err: err, message: `The admin could not be updated`});
+            res.json({message: 'Profile picture uploaded successfully'});
+        });
+
+    }
+    catch(exception){
+        res.json({err: error});
+    }
+   
 }
 
 adminService.prototype.createAccount = function(req, res, data){
-    var valid = validator.isValid(req, res, this.joiSchema, data);
-    if (valid != null){
-        res.json(valid);
-    }else{
-        repo.createAccount(data, function(err, adminAccount){
-            if(err) res.json({err: err, message: "Something went wrong, please try again"});
-            else{
+    try{
+            var valid = validator.isValid(req, res, this.joiSchema, data);
+            if (valid != null){
+                res.json(valid);
+            }else{
+                repo.createAccount(data, function(err, adminAccount){
+                    if(err) res.json({err: err, message: "Something went wrong, please try again"});
+                    else{
 
-                adminAccount.save();
-                res.json({sub: adminAccount, message: 'Your account has been created successfully'});
-            };
-        });
+                        adminAccount.save();
+                        res.json({sub: adminAccount, message: 'Your account has been created successfully'});
+                    };
+                });
+            }
     }
+
+    catch(exception){
+        res.json({err :error});
+    }
+    
 }
    
 module.exports = new adminService(joiSchema);
