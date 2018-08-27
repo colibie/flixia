52908f078b0fc3a40fa08e4dd3f7a397eaee87a0
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-27 11:58:24 +0100

    implemented try catch in the userService

--- Services/userService.js
+++ Services/userService.js
@@ -24,49 +24,68 @@ function userService(joiSchema){
 userService.prototype = baseService(repo);
 
 userService.prototype.uploadPicture = function(req, res, data){
-    repo.update(data._id, {profilePicture: data.profilePicture}, function(err, user){
-        if(err) res.json({err: err, message: `The user could not be updated`});
-        res.json({message: 'Profile picture uploaded successfully'});
-    });
+    try{
+            repo.update(data._id, {profilePicture: data.profilePicture}, function(err, user){
+            if(err) res.json({err: err, message: `The user could not be updated`});
+            res.json({message: 'Profile picture uploaded successfully'});
+        });
+    }
+    catch(exception){
+        res.json({err:error});
+    }
+   
 }
 
 userService.prototype.createAccount = function(req, res, data){
-    var valid = validator.isValid(req, res, this.joiSchema, data);
-    if (valid != null){
-        res.json(valid);
-    }else{
-        repo.createAccount(data, function(err, userAccount){
-            if(err) res.json({err: err, message: "Something went wrong, please try again"});
-            else{
-                sendMail(req, res, data.email, data.username);
-                userAccount.save();
-                res.json({sub: userAccount, message: 'Your account has been created successfully'});
-            };
-        });
+    try{
+        var valid = validator.isValid(req, res, this.joiSchema, data);
+        if (valid != null){
+            res.json(valid);
+        }else{
+            repo.createAccount(data, function(err, userAccount){
+                if(err) res.json({err: err, message: "Something went wrong, please try again"});
+                else{
+                    sendMail(req, res, data.email, data.username);
+                    userAccount.save();
+                    res.json({sub: userAccount, message: 'Your account has been created successfully'});
+                };
+            });
+        }
     }
+
+    catch(exception){
+        res.json({err:error});
+    }
+    
 }
 
 sendMail = function(req, res, userAccount, name){
-    // setup email data with unicode symbols
-    var mailOptions = {
-        from: 'helloflixia@gmail.com', // sender address
-        to: userAccount, // list of receivers
-        subject: `Welcome to Our World Of Nollywood Movies ${name} 🎇`, // Subject line
-        html: "<p>You are very welcome to our platform😁. Expect enough fun and updates from us.</p>"
-    };
+    try{
+ 
+        // setup email data with unicode symbols
+        var mailOptions = {
+            from: 'helloflixia@gmail.com', // sender address
+            to: userAccount, // list of receivers
+            subject: `Welcome to Our World Of Nollywood Movies ${name} 🎇`, // Subject line
+            html: "<p>You are very welcome to our platform😁. Expect enough fun and updates from us.</p>"
+        };
 
-    /**I need a function that ensures that email is sent
-     * else notify me of the failure to send email.
-     */
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(err){
-        if (err) {
-            res.json({message: 'Account could not be created'})
-        }
-        else{
-            console.log('Email sent successfully');
-        }
-    });
+        /**I need a function that ensures that email is sent
+         * else notify me of the failure to send email.
+         */
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function(err){
+            if (err) {
+                res.json({message: 'Account could not be created'})
+            }
+            else{
+                console.log('Email sent successfully');
+            }
+        });
+    }
+    catch(exception){
+        res.json({err:error});
+    }    
 }
     
 module.exports = new userService(joiSchema);
