ebfc707173b7551ff5466ddb3e1d039ed6a057b8
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-08-28 10:34:17 +0100

    Updated the trailer service to be able to delete files from cloudinary

--- Services/trailerService.js
+++ Services/trailerService.js
@@ -1,6 +1,8 @@
 var repo = require('../Repositories/trailerRepo');
 var baseService = require('../Services/baseService'); //contains the content of module.exports
 var joiSchema = require('../JoiSchema/trailerSchema');
+var cloud = require('../Config/cloudinary');
+
 
 function trailerService(joiSchema){
     //must be added for population purposes
@@ -12,5 +14,32 @@ function trailerService(joiSchema){
     this.joiSchema = joiSchema;
 }
 trailerService.prototype = baseService(repo);
+
+trailerService.prototype.deleteTrailer = function (req, res, id){
+    repo.getById(id,'','','', function(err, data)
+    {
+       try {
+           if (data != null){
+            repo.delete({_id:id}, function(err, result){
+                if (err) res.json({error: err, message: 'The data could not be deleted'});
+                else if (result == null){
+                    res.json({message: 'Resource does not exist'});
+                }else{
+                    cloud.deleteImage(data.trailerCoverId).then(()=>{
+                        cloud.deleteVideoFile(data.trailerVideoId) 
+                        res.json({message: 'Resource deleted successfully'});
+                            });                  
+                        }
+                    });
+             } else {
+               res.json({message: "Trailer not found, delete not successful"});
+           }
+        
+       } catch(exception){
+            //res.json({error : err});
+       }
+    })        
+};
+
 
 module.exports = new trailerService(joiSchema);
