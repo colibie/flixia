2f128f3737dc09aee6c16764446e0f1cbd0a6213
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-09-01 10:05:58 +0100

    created app comment shema for validation

---
+++ JoiSchema/appCommentSchema.js
@@ -0,0 +1,9 @@
+//This schema defines how joi validates data
+var Joi = require('joi');
+
+module.exports = Joi.object().keys({
+    content: Joi.string().required(),
+    time: Joi.any(),
+    email : Joi.string().email().required(),
+    name : Joi.string().required(),
+});
